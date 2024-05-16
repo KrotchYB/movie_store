d46b9097d57572f9c7902793994a71665eb93b31
burhanerden <brh.rdn@gmail.com>
2024-05-16 16:12:45 +0300

    Initial Commit

---
+++ CatalogService/src/main/java/MovieStoreApp/CatalogService/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package MovieStoreApp.CatalogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
---
+++ RecommendationService/src/test/java/MovieStoreApp/RecommendationService/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package MovieStoreApp.RecommendationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
